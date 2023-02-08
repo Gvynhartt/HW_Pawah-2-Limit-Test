cf5db05f7cf0776928ba3d81b16b934549244a94
Gvynhartt <savshush134@gmail.com>
2023-02-08 20:13:33 +0300

    csv specs fixed

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -7,7 +7,7 @@ import ru.netology.sqr.SQRService;
 public class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(0_hits).csv")
         // тестирует работу для диапазона с 0 возможных значений
     void shdCalcForZeroIntervalHits(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
@@ -16,7 +16,7 @@ public class SQRServiceTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(border_values).csv")
         //тестирует работу для диапазона, включающего крайниие значения из перебираемых чисел (т.е. 10 и 99)
     void shdCalcForIntervalLimitValues(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
@@ -25,7 +25,7 @@ public class SQRServiceTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(max_min).csv")
         //тестирует работу для случая, если перепутаны максимальное и минимальное значения
     void shdCalcForMixedUpLimitValues(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
@@ -34,7 +34,7 @@ public class SQRServiceTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(both_2nd_pow).csv")
         //тестирует работу при совпадении крайних значений диапазона, являющихся при этом квадратом
     void shdCalcForOverlappingLimitValuesThatAreSquare(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
@@ -43,7 +43,7 @@ public class SQRServiceTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(both_not_2_pow).csv")
         //тестирует работу при совпадении крайних значений диапазона, которые при этом квадратами НЕ являются
     void shdCalcForOverlappingLimitValuesThatAreNotSquare(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
@@ -52,7 +52,7 @@ public class SQRServiceTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/limit_test.csv")
+    @CsvFileSource(files = "src/test/resources/limit_test_(over_limits).csv")
         //тестирует поведение при границах диапазона, превосходящих разброс значений, т.е. за пределами [100;9801]
     void shdCalcForLimitValuesOutOfInterval(int lowerLimit, int upperLimit, int expectedHits) {
         SQRService count = new SQRService();
